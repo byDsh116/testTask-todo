40c9189f7560133b10377ce0b748ee75a12c5672
byDsh116 <by.dsh116@gmail.com>
2024-10-16 15:00:31 +0300

    create function: addTask

--- hooks/useTask.tsx
+++ hooks/useTask.tsx
@@ -1,9 +1,26 @@
-
+import { useState } from 'react'
 
 
 interface Task {
-    id:number,
+    id: number,
     text: string,
     completed: boolean;
 }
 
+export const useTask = () => {
+    const [tasks, setTasks] = useState<Task[]>([])
+    const [filter, setFilter] = useState<'all' | 'completed' | 'active'>('all')
+
+    const addTask = (taskText: string) => {
+        const newTask: Task = {
+            id: Date.now(),
+            text: taskText,
+            completed: false,
+        }
+
+        setTasks((prevTasks) => [...prevTasks, newTask])
+    }
+
+
+
+}
